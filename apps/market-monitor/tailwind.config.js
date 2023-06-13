23f97f438127a3ced60d273155bfff02390e2e05
Eduard Krivanek <krivanek1234@gmail.com>
2023-06-13 11:04:26 +0300

    chore: added tailwind

---
+++ apps/market-monitor/tailwind.config.js
@@ -0,0 +1,46 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['sans-serif', 'Poppins'],
+      serif: ['sans-serif', 'Poppins'],
+    },
+    extend: {
+      colors: {
+        'wt-background-dark': 'var(--background-dark)',
+        'wt-background-medium': 'var(--background-medium)',
+        'wt-background-light': 'var(--background-light)',
+
+        /* primary */
+        'wt-primary': 'var(--primary)',
+
+        /* success */
+        'wt-success': 'var(--success)',
+
+        /* danger */
+        'wt-danger': 'var(--danger)',
+
+        /* gray */
+        'wt-gray-dark': 'var(--gray-dark)',
+        'wt-gray-medium': 'var(--gray-medium)',
+        'wt-gray-light': 'var(--gray-light)',
+      },
+      flex: {
+        2: '2 2 0%',
+      },
+    },
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+};
