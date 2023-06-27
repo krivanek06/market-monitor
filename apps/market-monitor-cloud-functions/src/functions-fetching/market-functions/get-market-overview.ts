1f96a7bfaebd35be96287086958c22894bbeba2c
Eduard Krivanek <krivanek1234@gmail.com>
2023-06-27 15:36:46 +0300

    feat: created types from MARKET_OVERVIEW_DATABASE_KEYS

--- apps/market-monitor-cloud-functions/src/functions-fetching/market-functions/get-market-overview.ts
+++ apps/market-monitor-cloud-functions/src/functions-fetching/market-functions/get-market-overview.ts
@@ -1,6 +1,11 @@
 import { getQuandlDataFormatter, getTreasuryYieldUS } from '@market-monitor/api-external';
-import { MARKET_OVERVIEW_DATABASE_KEYS, getDatabaseMarketOverviewRef } from '@market-monitor/api-firebase';
-import { MarketOverviewData } from '@market-monitor/api-types';
+import { getDatabaseMarketOverviewRef } from '@market-monitor/api-firebase';
+import {
+  MARKET_OVERVIEW_DATABASE_ENDPOINTS,
+  MarketOverview,
+  MarketOverviewData,
+  MarketOverviewDatabaseKeys,
+} from '@market-monitor/api-types';
 import { Response } from 'express';
 import { zip } from 'lodash';
 
@@ -18,34 +23,43 @@ import { zip } from 'lodash';
 import { firestore } from 'firebase-admin';
 import { onRequest } from 'firebase-functions/v2/https';
 
-export const getmarketoverviewdata = onRequest(async (request, response: Response<MarketOverviewData | null | any>) => {
+export const getmarketoverview = onRequest(async (request, response: Response<MarketOverview | null>) => {});
+
+export const getmarketoverviewdata = onRequest(async (request, response: Response<MarketOverviewData | null>) => {
   // i.e: sp500
-  const key = request.query.key as keyof typeof MARKET_OVERVIEW_DATABASE_KEYS;
+  const key = request.query.key as MarketOverviewDatabaseKeys;
   // i.e: peRatio
-  const subKey = request.query.document as string;
+  const subKey = request.query.subKey as string;
   // whether to reload data from api or not
   const hardReload = request.query.hardReload as string;
 
+  const result = await loadMarketOverviewData(key, subKey, hardReload === 'true');
+
+  response.send(result);
+});
+
+// TODO: update data if older than 7 days
+const loadMarketOverviewData = async (
+  key: MarketOverviewDatabaseKeys,
+  subKey: string,
+  hardReload = false
+): Promise<MarketOverviewData | null> => {
   // get document and url from database: {qundal_treasury_yield_curve_rates_1_mo, USTREASURY/YIELD}
-  const { document, url } = MARKET_OVERVIEW_DATABASE_KEYS[key]?.[subKey] as { document: string; url: string };
+  const { document, url } = MARKET_OVERVIEW_DATABASE_ENDPOINTS[key]?.[subKey] as { document: string; url: string };
   console.log(key, subKey, document, url);
 
   if (!document || !url) {
     console.log(`no firebaseDocument found for key: [${key}] - [${document}]`);
-    response.send(null);
-    return;
+    return null;
   }
 
   // get data from firebase
   const marketOverviewRef = getDatabaseMarketOverviewRef(document);
   const marketOverviewData = (await marketOverviewRef.get()).data();
 
-  // TODO Update data
-
   // return data if exists
-  if (marketOverviewData && hardReload !== 'true') {
-    response.send(marketOverviewData);
-    return;
+  if (marketOverviewData && !hardReload) {
+    return marketOverviewData;
   }
 
   // resolve endpoint
@@ -54,8 +68,7 @@ export const getmarketoverviewdata = onRequest(async (request, response: Respons
   // save data to DB
   if (!(apiData instanceof Array)) {
     await updateSingleDocuments(document, apiData);
-    response.send(apiData);
-    return;
+    return apiData;
   }
 
   // get document keys to save multiple data into firestore
@@ -67,15 +80,15 @@ export const getmarketoverviewdata = onRequest(async (request, response: Respons
   // find the requested document and return it
   const indexToReturn = documentsToSave.findIndex((d) => d === document);
   const data = apiData[indexToReturn];
-  response.send(data);
-});
+  return data;
+};
 
 const loadDataFromEndpoint = (
   subkey: string, // i.e: peRatio
   url: string // i.e: MULTPL/SP500_PE_RATIO_MONTH
 ): Promise<MarketOverviewData | MarketOverviewData[]> => {
   // check if provided key is treasury
-  if (subkey in MARKET_OVERVIEW_DATABASE_KEYS.treasury) {
+  if (subkey in MARKET_OVERVIEW_DATABASE_ENDPOINTS.treasury) {
     return getTreasuryYieldUS();
   }
 
@@ -111,8 +124,8 @@ const updateMultipleDocuments = async (documents: string[], marketOverview: Mark
  * Example is `treasury` we get multiple data from the same url,
  * but we want to save them in different documents.
  */
-const getDocumentsToSaveData = (key: keyof typeof MARKET_OVERVIEW_DATABASE_KEYS, url: string): string[] => {
-  return Object.keys(MARKET_OVERVIEW_DATABASE_KEYS[key])
-    .filter((subKey) => MARKET_OVERVIEW_DATABASE_KEYS[key][subKey].url === url)
-    .map((subKey) => MARKET_OVERVIEW_DATABASE_KEYS[key][subKey].document);
+const getDocumentsToSaveData = (key: MarketOverviewDatabaseKeys, url: string): string[] => {
+  return Object.keys(MARKET_OVERVIEW_DATABASE_ENDPOINTS[key])
+    .filter((subKey) => MARKET_OVERVIEW_DATABASE_ENDPOINTS[key][subKey].url === url)
+    .map((subKey) => MARKET_OVERVIEW_DATABASE_ENDPOINTS[key][subKey].document);
 };
--- libs/api/api-types/src/lib/firebase-market-data.model.ts
+++ libs/api/api-types/src/lib/firebase-market-data.model.ts
@@ -20,6 +20,46 @@ export type FirebaseNewsTypes = (typeof firebaseNewsAcceptableTypes)[number];
 
 // ------------------ Market Overview ------------------
 
+export type MarketOverview = {
+  sp500: {
+    peRatio: MarketOverviewData[];
+    shillerPeRatio: MarketOverviewData[];
+    priceToBook: MarketOverviewData[];
+    priceToSales: MarketOverviewData[];
+    earningsYield: MarketOverviewData[];
+    dividendYield: MarketOverviewData[];
+  };
+  treasury: {
+    us1Month: MarketOverviewData[];
+    us3Month: MarketOverviewData[];
+    us1Year: MarketOverviewData[];
+    us5Year: MarketOverviewData[];
+    us10Year: MarketOverviewData[];
+    us30Year: MarketOverviewData[];
+  };
+  bonds: {
+    usAAAYield: MarketOverviewData[];
+    usAAYield: MarketOverviewData[];
+    usBBYield: MarketOverviewData[];
+    usCCCYield: MarketOverviewData[];
+    usCorporateYield: MarketOverviewData[];
+    usHighYield: MarketOverviewData[];
+    usEmergingMarket: MarketOverviewData[];
+  };
+  consumerPriceIndex: {
+    usCpi: MarketOverviewData[];
+    euCpi: MarketOverviewData[];
+    ukCpi: MarketOverviewData[];
+    gerCpi: MarketOverviewData[];
+  };
+  inflationRate: {
+    usInflationRate: MarketOverviewData[];
+    euInflationRate: MarketOverviewData[];
+    ukInflationRate: MarketOverviewData[];
+    gerInflationRate: MarketOverviewData[];
+  };
+};
+
 export type MarketOverviewData = {
   data: number[];
   dates: string[];
