10bbb946d9e97a709d05f933fa5e4734d8647587
Eduard Krivanek <krivanek1234@gmail.com>
2023-07-06 17:38:59 +0300

    chore: changed ScrollNearEndDirective

--- libs/shared/directives/src/lib/scroll-near-end.directive.ts
+++ libs/shared/directives/src/lib/scroll-near-end.directive.ts
@@ -1,23 +1,50 @@
-import { Directive, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Directive({
   selector: '[appScrollNearEnd]',
   standalone: true,
 })
 export class ScrollNearEndDirective implements OnInit {
-  @Input() threshold = 400;
-  private document!: HTMLElement;
-  ngOnInit(): void {
-    this.document = document.documentElement;
-  }
   @Output() nearEnd: EventEmitter<void> = new EventEmitter<void>();
 
-  @HostListener('document:mousewheel', ['$event.target'])
-  onScroll(target: HTMLElement): void {
-    const pos = (this.document.scrollTop || document.body.scrollTop) + this.document.offsetHeight;
-    const max = this.document.scrollHeight;
+  /**
+   * threshold in PX when to emit before page end scroll
+   */
+  @Input() threshold = 50;
 
-    if (max - pos < this.threshold) {
+  private window!: Window;
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    // save window object for type safety
+    this.window = window;
+  }
+
+  @HostListener('window:scroll', ['$event.target'])
+  windowScrollEvent(event: KeyboardEvent) {
+    // height of whole window page
+    const heightOfWholePage = this.window.document.documentElement.scrollHeight;
+
+    // how big in pixels the element is
+    const heightOfElement = this.el.nativeElement.scrollHeight;
+
+    // currently scrolled Y position
+    const currentScrolledY = this.window.scrollY;
+
+    // height of opened window - shrinks if console is opened
+    const innerHeight = this.window.innerHeight;
+
+    const spaceOfElementAndPage = heightOfWholePage - heightOfElement;
+
+    const scrollToBottom = heightOfElement - innerHeight - currentScrolledY + spaceOfElementAndPage;
+
+    // console.log('scrollToBottom:', scrollToBottom);
+
+    // console.log(currentScrolledY, innerHeight, heightOfWholePage, heightOfElement, spaceOfElementAndPage);
+
+    if (scrollToBottom < this.threshold) {
+      // console.log('%c [ScrollNearEndDirective]: emit', 'color: #bada55; font-size: 16px');
       this.nearEnd.emit();
     }
   }
