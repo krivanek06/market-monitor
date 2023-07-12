579b014b7368e3112c29350e13901ae5c56bf0b9
Eduard Krivanek <krivanek1234@gmail.com>
2023-07-12 13:31:22 +0300

    chore: generated project for cloudfalre interaction

---
+++ apps/market-monitor-cloudflare/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'market-monitor-cloudflare',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/market-monitor-cloudflare',
+};
