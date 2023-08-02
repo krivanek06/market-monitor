957daede58ad1f3695f67745629b078f366bb0a8
Eduard Krivanek <krivanek1234@gmail.com>
2023-08-02 21:45:06 +0300

    chore: show loading when loading stock details

--- apps/market-monitor/src/app/app.component.ts
+++ apps/market-monitor/src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterEvent,
+  RouterModule,
+} from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatProgressSpinnerModule],
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+    <main class="min-h-screen min-w-full">
+      <div *ngIf="loadingSignal(); else showRoute" class="grid place-content-center pb-[15%] min-h-screen min-w-full">
+        <mat-spinner></mat-spinner>
+      </div>
+
+      <ng-template #showRoute>
+        <router-outlet></router-outlet>
+      </ng-template>
+    </main>
+  `,
   styles: [
     `
       :host {
@@ -15,4 +35,26 @@ import { RouterModule } from '@angular/router';
     `,
   ],
 })
-export class AppComponent {}
+export class AppComponent {
+  loadingSignal = signal(false);
+
+  constructor(private router: Router) {
+    this.router.events.pipe(takeUntilDestroyed()).subscribe((routerEvent) => {
+      this.checkRouterEvent(routerEvent as RouterEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: RouterEvent): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loadingSignal.set(true);
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loadingSignal.set(false);
+    }
+  }
+}
