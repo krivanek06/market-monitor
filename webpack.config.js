d523341495c12fb49a2dd5d5490fd1222ec55b9d
Eduard Krivanek <krivanek1234@gmail.com>
2023-08-15 16:31:55 +0300

    chore: uploading sourcemap to sentry

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
+
+module.exports = {
+  // ... other config above ...
+
+  devtool: 'source-map', // Source map generation must be turned on
+  plugins: [
+    sentryWebpackPlugin({
+      org: 'eduard-krivanek',
+      project: 'javascript-angular',
+
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need the `project:releases` and `org:read` scopes
+      authToken: '509b66934fa78174a333cffdc71128f0cb7c2e4f2e0929ddde86d5e302094812',
+    }),
+  ],
+};
