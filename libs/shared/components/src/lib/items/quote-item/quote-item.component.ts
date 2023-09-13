a717146fbbb5fdb683e0f15e6fd43a60ba69e210
Eduard Krivanek <krivanek1234@gmail.com>
2023-09-13 19:38:45 +0200

    chore: ui fix:

--- libs/shared/components/src/lib/items/quote-item/quote-item.component.ts
+++ libs/shared/components/src/lib/items/quote-item/quote-item.component.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { SymbolQuote } from '@market-monitor/api-types';
 import { PercentageIncreaseDirective } from '@market-monitor/shared-directives';
-import { LargeNumberFormatterPipe } from '@market-monitor/shared-pipes';
+import { LargeNumberFormatterPipe, TruncatePipe } from '@market-monitor/shared-pipes';
 
 @Component({
   selector: 'app-quote-item',
   standalone: true,
-  imports: [CommonModule, PercentageIncreaseDirective, LargeNumberFormatterPipe],
+  imports: [CommonModule, PercentageIncreaseDirective, LargeNumberFormatterPipe, TruncatePipe],
   templateUrl: './quote-item.component.html',
   styleUrls: ['./quote-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,5 +15,4 @@ import { LargeNumberFormatterPipe } from '@market-monitor/shared-pipes';
 export class QuoteItemComponent {
   @Input({ required: true }) symbolQuote!: SymbolQuote;
   @Input() assetUrl?: string;
-  @Input() displaySecondLine = false;
 }
