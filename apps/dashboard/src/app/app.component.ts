b56f13dcf7210f575fbbc1028a2039f5b1f5ab63
Eduard Krivanek <krivanek1234@gmail.com>
2023-09-24 11:11:25 +0200

    feat: renamed app dashboard

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'market-monitor-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styles: [''],
 })
