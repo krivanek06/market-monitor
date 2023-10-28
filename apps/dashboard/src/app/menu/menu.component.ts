1a7643bf44cffa5676de7245ded03316fbd31203
Eduard Krivanek <krivanek1234@gmail.com>
2023-10-28 13:05:34 +0200

    chore: added loading when redirecting on stock details

--- apps/dashboard/src/app/menu/menu.component.ts
+++ apps/dashboard/src/app/menu/menu.component.ts
@@ -5,9 +5,11 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { SCREEN_LAYOUT } from '@market-monitor/shared/data-access';
+import { LoaderMainService } from '@market-monitor/shared/utils-client';
 import { map } from 'rxjs';
 import { MenuSideNavigationComponent } from './menu-side-navigation/menu-side-navigation.component';
 
@@ -22,8 +24,37 @@ import { MenuSideNavigationComponent } from './menu-side-navigation/menu-side-na
     RouterModule,
     MatDividerModule,
     MenuSideNavigationComponent,
+    MatProgressSpinnerModule,
   ],
-  templateUrl: './menu.component.html',
+  template: `
+    <mat-drawer-container autosize class="h-full">
+      <!-- side nav -->
+      <mat-drawer
+        [mode]="isSmallScreenSignal() ? 'over' : 'side'"
+        [opened]="true"
+        class="w-2/12 min-w-[275px]"
+        role="navigation"
+        (closed)="toggleMatDrawerExpandedView()"
+      >
+        <app-menu-side-navigation></app-menu-side-navigation>
+      </mat-drawer>
+
+      <mat-drawer-content>
+        <div class="px-10 pt-6 c-content-wrapper">
+          <!-- content -->
+          <div *ngIf="loadingSignal()" class="grid place-content-center pb-[15%] min-h-screen min-w-full">
+            <mat-spinner></mat-spinner>
+          </div>
+
+          <div [ngClass]="{ hidden: loadingSignal() }">
+            <router-outlet></router-outlet>
+          </div>
+          <!-- footer for additional space on bottom -->
+          <footer class="w-full h-12"></footer>
+        </div>
+      </mat-drawer-content>
+    </mat-drawer-container>
+  `,
   styles: [
     `
       :host {
@@ -53,6 +84,7 @@ import { MenuSideNavigationComponent } from './menu-side-navigation/menu-side-na
 export class MenuComponent implements OnInit {
   breakpointObserver = inject(BreakpointObserver);
 
+  loadingSignal = toSignal(inject(LoaderMainService).getLoading());
   isOpenSignal = signal<boolean>(false);
   isSmallScreenSignal = toSignal(
     this.breakpointObserver.observe([SCREEN_LAYOUT.LAYOUT_SM]).pipe(map((x) => !x.matches)),
