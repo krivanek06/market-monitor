6685efeedb9dab96715d707c868adf6462b35287
Eduard Krivanek <krivanek1234@gmail.com>
2023-12-16 13:16:03 +0100

    feat(build): removed ssr

--- apps/market-monitor/src/app/app.config.ts
+++ apps/market-monitor/src/app/app.config.ts
@@ -1,6 +1,5 @@
-import { isPlatformServer } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
-import { APP_ID, ApplicationConfig, PLATFORM_ID, PLATFORM_INITIALIZER, inject } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
@@ -11,8 +10,6 @@ import {
   withPreloading,
 } from '@angular/router';
 // import * as Sentry from '@sentry/angular-ivy';
-import { API_IS_PRODUCTION } from '@market-monitor/api-client';
-import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -28,32 +25,12 @@ export const appConfig: ApplicationConfig = {
       withEnabledBlockingInitialNavigation(),
       // in place of preloadingStrategy: PreloadAllModules
       withPreloading(PreloadAllModules),
+      // add transition animations
+      // withViewTransitions(),
     ),
     provideAnimations(),
     provideClientHydration(),
     // withNoHttpTransferCache(),
-    { provide: APP_ID, useValue: 'serverApp' },
-    {
-      provide: PLATFORM_INITIALIZER,
-      useFactory: () => {
-        const isServer = isPlatformServer(inject(PLATFORM_ID));
-        // we don't want to inject the script on the server side
-        if (!isServer) {
-          window.console.log = () => {
-            /* disable console log on prod */
-          };
-
-          // inject analytics
-          // injectAnalyticsScript({ gaTrackId: ANALYTICS_TRACK_ID });
-          // trackRouterEvents();
-        }
-      },
-      multi: true,
-    },
-    {
-      provide: API_IS_PRODUCTION,
-      useValue: environment.production,
-    },
     // {
     //   provide: ErrorHandler,
     //   useValue: Sentry.createErrorHandler({
--- libs/api/api-client/src/lib/utils/api.model.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const API_IS_PRODUCTION = new InjectionToken<boolean>('API_IS_PRODUCTION');
