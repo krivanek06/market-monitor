10ecdd521d1d59aa4177e200ba951aa73e853e24
Eduard Krivanek <krivanek1234@gmail.com>
2024-01-27 18:35:49 +0100

    chore: changed value-presentation-button

--- libs/shared/ui/src/lib/components/form-controls/value-presentation-button-control/value-presentation-button-control.component.ts
+++ libs/shared/ui/src/lib/components/form-controls/value-presentation-button-control/value-presentation-button-control.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, Input, OnInit, signal } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { ValuePresentItem } from '@market-monitor/shared/data-access';
@@ -8,8 +8,55 @@ import { InArrayPipe } from '../../../pipes';
 
 @Component({
   selector: 'app-value-presentation-button-control',
-  templateUrl: './value-presentation-button-control.component.html',
-  styleUrls: ['./value-presentation-button-control.component.scss'],
+  template: `
+    <div class="flex" [ngClass]="{ 'flex-col': !isFlexRow }">
+      <button
+        *ngFor="let item of items"
+        mat-button
+        [ngClass]="{ 'w-max': isFlexRow }"
+        [style.--valueItemColor]="item.color"
+        [style.--valueItemColorActive]="item.color + '44'"
+        [style]="(activeItems() | inArray: item.item : itemKey) ? 'background-color: ' + item.color + '44' : ''"
+        (click)="onClick(item.item)"
+        class="h-16"
+      >
+        <div class="flex flex-col">
+          <div class="flex flex-row gap-2">
+            <img *ngIf="item.imageSrc as imageSrc" appDefaultImg [src]="imageSrc" />
+            <div class="mr-2 text-lg text-wt-gray-light">{{ item.name }}</div>
+          </div>
+          <div class="flex flex-row gap-2">
+            <div class="text-base text-wt-gray-medium">{{ item.value | currency }}</div>
+            <div *ngIf="item.valuePrct" class="text-base text-wt-gray-dark">
+              ({{ item.valuePrct | percent: '1.2-2' }})
+            </div>
+          </div>
+        </div>
+      </button>
+    </div>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+
+        img {
+          height: 24px;
+        }
+
+        button {
+          @apply px-4 p-1 rounded-lg m-1;
+
+          border: 1px solid var(--valueItemColor);
+          border-left: 4px solid var(--valueItemColor);
+          border-right: 4px solid var(--valueItemColor);
+          &:hover {
+            background-color: var(--valueItemColorActive);
+          }
+        }
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [CommonModule, MatButtonModule, DefaultImgDirective, InArrayPipe],
@@ -32,7 +79,7 @@ export class ValuePresentationButtonControlComponent<T> implements OnInit, Contr
    */
   @Input() selectWholeItem = true;
 
-  activeItems: T[] = [];
+  activeItems = signal<T[]>([]);
 
   /**
    * parent can be notified by the whole object <T[]> or by its key <T[keyof T][]>
@@ -45,28 +92,28 @@ export class ValuePresentationButtonControlComponent<T> implements OnInit, Contr
   ngOnInit(): void {}
 
   onClick(item: T): void {
-    const inArray = this.activeItems.find((d) => d[this.itemKey] === item[this.itemKey]);
+    const inArray = this.activeItems().find((d) => d[this.itemKey] === item[this.itemKey]);
     if (inArray) {
-      this.activeItems = this.activeItems.filter((d) => d[this.itemKey] !== item[this.itemKey]);
+      this.activeItems.set(this.activeItems().filter((d) => d[this.itemKey] !== item[this.itemKey]));
     } else {
       if (!this.multiSelect) {
-        this.activeItems = [item];
+        this.activeItems.set([item]);
       } else {
-        this.activeItems = [...this.activeItems, item];
+        this.activeItems.set([...this.activeItems(), item]);
       }
     }
 
     // notify parent
     if (this.selectWholeItem) {
-      this.onChange(this.activeItems);
+      this.onChange(this.activeItems());
     } else {
-      const keyValue = this.activeItems.map((d) => d[this.itemKey]);
+      const keyValue = this.activeItems().map((d) => d[this.itemKey]);
       this.onChange(keyValue);
     }
   }
 
   writeValue(items: T[]): void {
-    this.activeItems = [...items];
+    this.activeItems.set([...items]);
   }
 
   /**
