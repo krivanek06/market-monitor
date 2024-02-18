e6d2f9cc73bc3058d2650d25d9f02e002fd9a99a
Eduard Krivanek <krivanek1234@gmail.com>
2024-02-18 09:42:05 +0100

    chore: migrated modules-user-ui

--- libs/modules/user/ui/src/lib/components/user-display-item/user-display-item.component.ts
+++ libs/modules/user/ui/src/lib/components/user-display-item/user-display-item.component.ts
@@ -4,22 +4,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { USER_ACTIVE_ACCOUNT_TIME_DAYS_LIMIT, UserBase } from '@market-monitor/api-types';
 import { DefaultImgDirective, LargeNumberFormatterPipe, PercentageIncreaseDirective } from '@market-monitor/shared/ui';
 import { isBefore, subDays } from 'date-fns';
+import { input } from '@angular/core';
 
 @Component({
   selector: 'app-user-display-item',
   standalone: true,
   imports: [CommonModule, DefaultImgDirective, LargeNumberFormatterPipe, MatIconModule, PercentageIncreaseDirective],
+
   template: `
     <div class="flex gap-4">
-      <img appDefaultImg [src]="userData.personal.photoURL" alt="User Image" class="w-14 h-14 rounded-md" />
+      <img appDefaultImg [src]="userData().personal.photoURL" alt="User Image" class="w-14 h-14 rounded-md" />
 
       <!-- info -->
       <div class="flex flex-col text-sm">
         <div class="flex">
           <div class="text-wt-gray-dark w-[80px]">Name:</div>
-          <div class="mr-4">{{ userData.personal.displayName }}</div>
+          <div class="mr-4">{{ userData().personal.displayName }}</div>
           <!-- active user -->
-          <mat-icon *ngIf="showLoginButton" [color]="isUserActive ? 'accent' : 'warn'" class="hidden sm:block">
+          <mat-icon *ngIf="showLoginButton()" [color]="isUserActive ? 'accent' : 'warn'" class="hidden sm:block">
             radio_button_checked
           </mat-icon>
         </div>
@@ -27,12 +29,12 @@ import { isBefore, subDays } from 'date-fns';
         <div class="flex">
           <div class="text-wt-gray-dark w-[80px]">Balance:</div>
           <div class="flex items-center gap-2">
-            <div>{{ userData.portfolioState.balance | largeNumberFormatter: false : true }}</div>
+            <div>{{ userData().portfolioState.balance | largeNumberFormatter: false : true }}</div>
             <div
-              *ngIf="userData.portfolioState.previousBalanceChangePercentage > 0"
+              *ngIf="userData().portfolioState.previousBalanceChangePercentage > 0"
               appPercentageIncrease
               [changeValues]="{
-                changePercentage: userData.portfolioState.previousBalanceChangePercentage
+                changePercentage: userData().portfolioState.previousBalanceChangePercentage
               }"
             ></div>
           </div>
@@ -40,7 +42,7 @@ import { isBefore, subDays } from 'date-fns';
 
         <div class="flex">
           <div class="text-wt-gray-dark w-[80px]">Login:</div>
-          <span>{{ userData.lastLoginDate | date: 'MMMM d, y' }}</span>
+          <span>{{ userData().lastLoginDate | date: 'MMMM d, y' }}</span>
         </div>
       </div>
     </div>
@@ -53,12 +55,12 @@ import { isBefore, subDays } from 'date-fns';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserDisplayItemComponent {
-  @Input({ required: true }) userData!: UserBase;
-  @Input() showLoginButton = true;
+  userData = input.required<UserBase>();
+  showLoginButton = input(true);
 
   USER_ACTIVE_ACCOUNT_TIME_DAYS = USER_ACTIVE_ACCOUNT_TIME_DAYS_LIMIT;
 
   get isUserActive(): boolean {
-    return isBefore(subDays(new Date(), USER_ACTIVE_ACCOUNT_TIME_DAYS_LIMIT), new Date(this.userData.lastLoginDate));
+    return isBefore(subDays(new Date(), USER_ACTIVE_ACCOUNT_TIME_DAYS_LIMIT), new Date(this.userData().lastLoginDate));
   }
 }
