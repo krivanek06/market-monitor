3f3b291befc3aecf15bb65406349244a755edcf7
Eduard Krivanek <krivanek1234@gmail.com>
2024-02-21 19:53:55 +0100

    feat: generated nx project

---
+++ apps/website/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import { fileURLToPath } from 'node:url';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: '../../dist/apps/website',
+  integrations: [
+    tailwind({
+      configFile: fileURLToPath(new URL('./tailwind.config.cjs', import.meta.url)),
+    }),
+  ],
+});
