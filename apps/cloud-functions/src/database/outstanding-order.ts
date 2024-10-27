6d61b4458dcca11716592e9e5a0b92d7410a983f
Eduard Krivanek <krivanek1234@gmail.com>
2024-10-27 10:31:20 +0100

    chore(auth-delete): remove outstanding orders on account delete for the user

--- apps/cloud-functions/src/database/outstanding-order.ts
+++ apps/cloud-functions/src/database/outstanding-order.ts
@@ -5,4 +5,7 @@ import { assignTypes } from './assign-type';
 export const outstandingOrderCollectionRef = () =>
   firestore().collection('outstanding_orders').withConverter(assignTypes<OutstandingOrder>());
 
+export const outstandingOrderCollectionByUserRef = (userId: string) =>
+  outstandingOrderCollectionRef().where('userData.id', '==', userId);
+
 export const outstandingOrderDocRef = (orderId: string) => outstandingOrderCollectionRef().doc(orderId);
