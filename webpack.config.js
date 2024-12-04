b5179cfd9dd2b29e3859d783fd6d0897e62297df
Eduard Krivanek <krivanek1234@gmail.com>
2024-12-04 11:30:06 +0100

    chore: removed webpack

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
-
-module.exports = {
-  mode: 'none',
-
-  // sentry config
-  devtool: 'source-map', // Source map generation must be turned on
-  plugins: [],
-};
