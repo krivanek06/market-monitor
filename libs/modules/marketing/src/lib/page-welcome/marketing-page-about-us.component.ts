62f0fa0671eb7c3c548edb9e47c5232594647275
Eduard Krivanek <krivanek1234@gmail.com>
2025-01-27 08:07:57 +0100

    chore: small fix

--- libs/modules/marketing/src/lib/page-welcome/marketing-page-about-us.component.ts
+++ libs/modules/marketing/src/lib/page-welcome/marketing-page-about-us.component.ts
@@ -6,7 +6,7 @@ import { MarketingSocialsEduardComponent } from '../components';
   imports: [MarketingSocialsEduardComponent],
   template: `
     <section class="mx-auto grid place-content-center lg:w-8/12">
-      <h2 class="g-section-title">About us</h2>
+      <h2 class="g-section-title">About Us</h2>
       <p class="mb-10 text-center text-xl text-gray-300">
         My name is Eduard Krivanek, and I am the sole developer behind GGFinance. With a passion for education and
         finance, I created this platform to provide students with a practical and engaging way to learn about the
@@ -26,9 +26,9 @@ import { MarketingSocialsEduardComponent } from '../components';
       <div class="mb-10 text-center text-xl text-gray-300 [&>*]:mb-2">
         <div>Schools where <span class="text-wt-primary">GGFinance</span> is used are:</div>
         <div>Univezita Komenského v Bratislave</div>
-        <div>Ekonimická Univerzita v Bratislav</div>
-        <div>Prešovská univerzita v Prešove</div>
-        <div>Univerzita Mateja Bela v Banskej Bystrici</div>
+        <div>Ekonimická Univerzita v Bratislave</div>
+        <!-- <div>Prešovská univerzita v Prešove</div>
+        <div>Univerzita Mateja Bela v Banskej Bystrici</div> -->
       </div>
     </section>
   `,
