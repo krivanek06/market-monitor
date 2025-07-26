f35848fee22a4771e3ea7da39d1813ba7305beec
Eduard Krivanek <krivanek1234@gmail.com>
2025-07-26 08:50:24 +0200

    chore: removed unused api keys

--- apps/dashboard/src/environments/environment.prod.ts
+++ apps/dashboard/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'market-monitor-prod',
-    appId: '1:185409661855:web:0c3e2fbdd4f05709f6ebaa',
-    storageBucket: 'market-monitor-prod.appspot.com',
-    apiKey: 'AIzaSyAv5SuzwrwGv0Z5RzFYHf5bdkjb60ucOAk',
-    authDomain: 'market-monitor-prod.firebaseapp.com',
-    messagingSenderId: '185409661855',
-    measurementId: 'G-82FD6D7MMR',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
 
   production: true,
--- libs/api/api-external/src/lib/environments.ts
+++ libs/api/api-external/src/lib/environments.ts
@@ -1,12 +1,12 @@
 // environments
-export const FINANCIAL_MODELING_KEY = '645c1db245d983df8a2d31bc39b92c32';
+export const FINANCIAL_MODELING_KEY = 'API_KEY'; // Replace with your actual API key
 export const FINANCIAL_MODELING_URL = 'https://financialmodelingprep.com';
 export const FINANCIAL_MODELING_URL_API = 'https://financialmodelingprep.com/api';
 
 /*
  https://finnhub.io/docs/api - 60 requests / minute
 */
-export const FINNHUB_KEY = 'ccde54iad3ibmia85amg';
+export const FINNHUB_KEY = 'API_KEY'; // Replace with your actual API key
 export const FINNHUB_URL = 'https://finnhub.io/api';
 
 export const QUANDL_KEY = 'Q6H1TuFXrV4_BspmjRoK'; // 'SKd8wxy69Zx-dEsoWP2C';
